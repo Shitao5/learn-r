fd9c94efa3c778db66e75fdaf43e1602d28d6daf
shitao <wuchouchouer@163.com>
2022-08-04 09:03:36 +0800

    common.R

--- common.R
+++ common.R
@@ -3,11 +3,11 @@
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
-  cache = FALSE
+  cache = FALSE,
   # fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
   # dpi = 300,
   # out.width = "70%",
-  # fig.align = 'center',
+  fig.align = 'center'
   # fig.width = 6,
   # fig.asp = 0.618,  # 1 / phi
   # fig.show = "hold"
